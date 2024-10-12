0df163a5327faedd2d140a667d094abb8cc6f956
PRITISH TOMAR <pritishtomar2004@gmail.com>
2024-10-12 22:05:41 +0530

    bug fixed

--- Routes/Route.js
+++ Routes/Route.js
@@ -3,7 +3,9 @@ const { saveUserData, showAllUSer} = require('../Controllers/User')
 
 const router = express.Router();
 
-
+router.get("/", (req, res)=>{
+    res.send("Ping from the server!")
+})
 router.post('/user', saveUserData);
 router.get('/user', showAllUSer);
 
